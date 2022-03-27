4fe86d4e86c99eae1507c7be575fac48e6c3eac9
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2022-03-27 11:49:54 +0700

    for

--- installasi_dart.dart
+++ installasi_dart.dart
@@ -1,21 +1,26 @@
-import 'dart:io';
+// import 'dart:io';
 
 void main(List<String> args) {
-  int number = int.tryParse(stdin.readLineSync());
 
-  switch (number) {
-    case 0:
-      print("nol");
-      break;
-    case 1:
-      print("satu");
-      break;
-    case 2:
-      print("dua");
-      break;
-    default:
-      print("bilangan lainya");
-  }
+    for (int counter = 0; counter < 5; counter = counter + 1) {
+      print("hello world!");
+    }
+
+  // int number = int.tryParse(stdin.readLineSync());
+
+  // switch (number) {
+  //   case 0:
+  //     print("nol");
+  //     break;
+  //   case 1:
+  //     print("satu");
+  //     break;
+  //   case 2:
+  //     print("dua");
+  //     break;
+  //   default:
+  //     print("bilangan lainya");
+  // }
 
   // 4
   // if (number == 0) {
