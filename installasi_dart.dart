5dd0d378ed2ca96690efc6b27cba4a6bbece4e34
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2022-03-27 11:21:11 +0700

    if else input

--- installasi_dart.dart
+++ installasi_dart.dart
@@ -2,12 +2,22 @@ import 'dart:io';
 
 void main(List<String> args) {
   int number = int.tryParse(stdin.readLineSync());
+  String output;
 
   if (number > 0) {
-    print("positif");
-  } else if (number < 0) {
-    print("negatif");
+    output = "positif";
   } else {
-    print("nol");
+    output = "negatif atau nol";
   }
+
+  print(output);
+
+// 1.
+  // if (number > 0) {
+  //   print("positif");
+  // } else if (number < 0) {
+  //   print("negatif");
+  // } else {
+  //   print("nol");
+  // }
 }
