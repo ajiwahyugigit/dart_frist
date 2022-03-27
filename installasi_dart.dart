5fad377d35932d8000ae063ce69d156e60df5fb4
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2022-03-27 12:03:32 +0700

    pengulangan do dan while

--- installasi_dart.dart
+++ installasi_dart.dart
@@ -1,10 +1,23 @@
 // import 'dart:io';
 
 void main(List<String> args) {
+  int i = 5;
+  while (i < 3) {
+    print("hallo ke " + i.toString());
+    i += 1;
+  }
 
-    for (int counter = 0; counter < 5; counter = counter + 1) {
-      print("hello world!");
-    }
+  print("========");
+
+  int x = 5;
+  do {
+    print("hello ke " + x.toString());
+    x += 1;
+  } while (x < 5);
+
+  // for (int counter = 0; counter < 5; counter = counter + 1) {
+  //   print("hello world!");
+  // }
 
   // int number = int.tryParse(stdin.readLineSync());
 
