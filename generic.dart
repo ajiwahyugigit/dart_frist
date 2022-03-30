8a21815e4c52d2960da8e6c916b601a78742dbbc
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2022-03-30 13:00:50 +0700

    generics

--- generic.dart
+++ generic.dart
@@ -1,8 +1,9 @@
 import 'package:installasi_dart/dateTime.dart';
 import 'package:installasi_dart/int_generic.dart';
+import 'package:installasi_dart/secure_box.dart';
 
 void main(List<String> args) {
-  var box = DateTimeSecureBox(DateTime.now(), '123');
+  var box = SecureBox<int>(123, '123');
 
   print(box.getData('123'));
 }
